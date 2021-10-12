1dcf119c6de9fdee4462583ca494824dc2844490
SProga <shane.306@hotmail.com>
2021-10-12 14:07:52 -0400

    refactored tags v-4

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,65 +8,65 @@ import React from "react";
 import CartIcon from "../Cart/CartIcon/CartIcon";
 
 const Navigation = (props) => {
-  const [isActive, setIsActive] = useState(false);
-  const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
+	const [isActive, setIsActive] = useState(false);
+	const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const showLoginHandler = (e) => {
-    e.preventDefault();
-    dispatch(loginActions.toggle());
-  };
+	const showLoginHandler = (e) => {
+		e.preventDefault();
+		dispatch(loginActions.toggle());
+	};
 
-  const toggleMenuHandler = () => {
-    setIsActive((active) => !active);
-  }; //this function is used to toggle the menu
+	const toggleMenuHandler = () => {
+		setIsActive((active) => !active);
+	}; //this function is used to toggle the menu
 
-  const logOutHandler = (e) => {
-    e.preventDefault();
-    dispatch(loginActions.logOut());
-  };
+	const logOutHandler = (e) => {
+		e.preventDefault();
+		dispatch(loginActions.logOut());
+	};
 
-  const styleArr = ["nav--right", isActive ? "fadeIn" : ""];
-  const navigationStyles = styleArr.map((style) => {
-    return classes[style];
-  });
+	const styleArr = ["nav--right", isActive ? "fadeIn" : ""];
+	const navigationStyles = styleArr.map((style) => {
+		return classes[style];
+	});
 
-  return (
-    <nav className={classes.nav}>
-      <Logo />
-      <CartIcon />
-      <div className={classes["nav--collapsible"]}>
-        <button
-          className={`hamburger hamburger--elastic ${classes["nav-menuBtn"]} ${
-            isActive ? "is-active" : ""
-          }`}
-          type="button"
-          onClick={toggleMenuHandler}
-        >
-          <span className="hamburger-box">
-            <span className="hamburger-inner"></span>
-          </span>
-        </button>
+	return (
+		<nav className={classes.nav}>
+			<Logo />
+			<CartIcon />
+			<div className={classes["nav--collapsible"]}>
+				<button
+					className={`hamburger hamburger--elastic ${classes["nav-menuBtn"]} ${
+						isActive ? "is-active" : ""
+					}`}
+					type="button"
+					onClick={toggleMenuHandler}>
+					<span className="hamburger-box">
+						<span className="hamburger-inner"></span>
+						<span className="sr-only">Toggle Navigation</span>
+					</span>
+				</button>
 
-        <div className={navigationStyles.join(" ")}>
-          <a href="/">New Arrivals</a>
-          <a href="/">Reviews</a>
-          <a href="/">About</a>
-          {!isLoggedIn && (
-            <a href="/" className={classes.btn} onClick={showLoginHandler}>
-              Login
-            </a>
-          )}
-          {isLoggedIn && (
-            <a href="/" className={classes.btn} onClick={logOutHandler}>
-              LogOut
-            </a>
-          )}
-        </div>
-      </div>
-    </nav>
-  );
+				<div className={navigationStyles.join(" ")}>
+					<a href="/">New Arrivals</a>
+					<a href="/">Reviews</a>
+					<a href="/">About</a>
+					{!isLoggedIn && (
+						<a href="/" className={classes.btn} onClick={showLoginHandler}>
+							Login
+						</a>
+					)}
+					{isLoggedIn && (
+						<a href="/" className={classes.btn} onClick={logOutHandler}>
+							LogOut
+						</a>
+					)}
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navigation;
