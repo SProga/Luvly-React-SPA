41a1e5b1fd41975cbb3806ab8bd9d239b5368df1
Shane Proverbs <shane.306@hotmail.com>
2021-10-23 15:21:16 -0400

    Add files via upload

---
+++ src/toFirebase/data.js
@@ -0,0 +1,117 @@
+export const reviews = [
+  {
+    id: "p1",
+    amount: 300,
+    text: "sets Sold",
+  },
+  {
+    id: "p2",
+    amount: 400,
+    text: "satisfied Customers",
+  },
+  {
+    id: "p3",
+    amount: 7,
+    text: "days a week",
+  },
+];
+
+export const products = [
+  {
+    id: "prd1",
+    name: "Contemporary blue ",
+    src: "https://ik.imagekit.io/edfgw98aksz/blue_couch_-GmkuiJnB.png?updatedAt=1628034132881",
+    price: 54.99,
+  },
+  {
+    id: "prd2",
+    name: "Comfy Queen Bed ",
+    src: "https://ik.imagekit.io/edfgw98aksz/black_bed_BtTDhyknF.png?updatedAt=1628034181706",
+    price: 1124.99,
+  },
+  {
+    id: "prd3",
+    name: "Traditional wooden square ",
+    src: "https://ik.imagekit.io/edfgw98aksz/sexycouch_dIdskK-WZxF.png?updatedAt=1628034252194",
+    price: 354.99,
+  },
+];
+
+export const CategoryData = [
+  {
+    id: "c1",
+    category: "chairs",
+    name: "Chairs",
+    src: "https://ik.imagekit.io/edfgw98aksz/gray_soft_seat_YiaMmEiYKO9.png?updatedAt=1628034296621",
+  },
+  {
+    id: "c2",
+    category: "sofa",
+    name: "Sofas",
+    src: "https://ik.imagekit.io/edfgw98aksz/brown_sofa_qP4mi9A6PQ7.png?updatedAt=1628034344361",
+  },
+  {
+    id: "c3",
+    category: "beds",
+    name: "Beds",
+    src: "https://ik.imagekit.io/edfgw98aksz/brown_bed_5IWVKSSaNOl.png?updatedAt=1628034380966",
+  },
+  {
+    id: "c4",
+    category: "tables",
+    name: "tables",
+    src: "https://ik.imagekit.io/edfgw98aksz/pngegg_ayumseBMhKU.png?updatedAt=1628034515216",
+  },
+];
+
+export const productCat = [
+  {
+    id: "pc1",
+    name: "beige soft seated chair",
+    src: "https://ik.imagekit.io/edfgw98aksz/nice_chair_CH-1btRP0hT.png?updatedAt=1628034563093",
+    price: 78.99,
+    category: "chairs",
+  },
+  {
+    id: "pc2",
+    name: "executives only",
+    src: "https://ik.imagekit.io/edfgw98aksz/executives_vzJTKcUw0oLQ.png?updatedAt=1628034600619",
+    price: 299.99,
+    category: "chairs",
+  },
+  {
+    id: "pc3",
+    name: "outdoor luxury",
+    src: "https://ik.imagekit.io/edfgw98aksz/black_chair_iiDZNadPSBP.png?updatedAt=1628034642020",
+    price: 124.49,
+    category: "chairs",
+  },
+  {
+    id: "pc4",
+    name: "blue steele decorative chair",
+    src: "https://ik.imagekit.io/edfgw98aksz/blue_chair_77aAIOin1_P.png?updatedAt=1628034673728",
+    price: 78.99,
+    category: "chairs",
+  },
+  {
+    id: "pc5",
+    name: "Mahogany Mixee Table",
+    src: "https://ik.imagekit.io/edfgw98aksz/mahogany_table_3z_Jj9giXmA.png?updatedAt=1628034725293",
+    price: 2244.99,
+    category: "tables",
+  },
+  {
+    id: "pc6",
+    name: "Antique Wooden Box Tea Table",
+    src: "https://ik.imagekit.io/edfgw98aksz/furnished_table_mwR_PSAL8Ipbf.png?updatedAt=1628034760415",
+    price: 1244.99,
+    category: "tables",
+  },
+  {
+    id: "pc7",
+    name: "Canpony Bed",
+    src: "https://ik.imagekit.io/edfgw98aksz/canopy_bed_CAvlIa5bLWb.png?updatedAt=1628034786026",
+    price: 1500.0,
+    category: "beds",
+  },
+];
